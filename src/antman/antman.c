3f3d8e676686dee63623cc85b15aa49a01a0bc99
Nicolasalx <nicolas1.alexandre@epitech.eu>
2023-01-02 14:27:20 +0100

    [ADD] Makefile and structure

---
+++ src/antman/antman.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** antman
+** File description:
+** antman
+*/
+
+#include "my.h"
+
+int main(void)
+{
+    printf("test");
+}
---
+++ src/giantman/giantman.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** giantman
+** File description:
+** giantman
+*/
+
+#include "my.h"
+
+int main(void)
+{
+    printf("test");
+}
